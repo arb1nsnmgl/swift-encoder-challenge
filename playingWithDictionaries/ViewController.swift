e9b171a752f8229c8d22a5dfcb0076a7d74cf8fa
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-09-19 16:13:32 -0400

    Done

--- playingWithDictionaries/ViewController.swift
+++ playingWithDictionaries/ViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var textField: UITextField!
+class ViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var textLabel: UILabel!
     
-    var code: [String:String] = [:]
+    @IBOutlet weak var decodeButton: UIButton!
+    @IBOutlet weak var encodeButton: UIButton!
+    @IBOutlet weak var offsetButton: UIButton!
+    
+    @IBOutlet weak var msgTextField: UITextField!
+    
+    @IBOutlet weak var blurWindow: UIVisualEffectView!
+    @IBOutlet weak var displayMessage: UILabel!
+    
+    var code = [String : String]()
+    var alphabet = [String]()
+    var newAlphabetDict = [String : String]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
         
         code = [
             "a" : "🐶",
@@ -28,7 +38,7 @@ class ViewController: UIViewController {
             "e" : "🐰",
             "f" : "🐻",
             "g" : "🐼",
-            "h" : "🐨",
+            "h" : "🐨", // 🐨🐰🐮🐮🐸
             "i" : "🐯",
             "j" : "🦁",
             "k" : "🦁",
@@ -49,73 +59,159 @@ class ViewController: UIViewController {
             "z" : "🐺"
         ]
         
-//        self.code = [
-//            "a" : "b",
-//            "b" : "c",
-//            "c" : "d",
-//            "d" : "e",
-//            "e" : "f",
-//            "f" : "g",
-//            "g" : "h",
-//            "h" : "i",
-//            "i" : "j",
-//            "j" : "k",
-//            "k" : "l",
-//            "l" : "m",
-//            "m" : "n",
-//            "n" : "o",
-//            "o" : "p",
-//            "p" : "q",
-//            "q" : "r",
-//            "r" : "s",
-//            "s" : "t",
-//            "t" : "u",
-//            "u" : "v",
-//            "v" : "w",
-//            "w" : "x",
-//            "x" : "y",
-//            "y" : "z",
-//            "z" : "a"
-//        ]
- 
-
-    }
-
-    @IBAction func convertBtnPressed(_ sender: AnyObject) {
+        msgTextField.becomeFirstResponder()
+        blurWindow.isHidden = true
+        blurWindow.layer.cornerRadius = 5
+        blurWindow.layer.masksToBounds = true
         
-        //get textfield value through self.textfield
-        textLabel.text = self.textField.text
+        decodeButton.isEnabled = false
+        encodeButton.isEnabled = false
+        offsetButton.isEnabled = false
         
-        var encodedMessage = ""
-        
-        var spyMessage = self.textField.text
-        if let unwrappedSpyMessage = self.textField.text{
-            var lowerCaseMessage = unwrappedSpyMessage.lowercased()
-            for char in lowerCaseMessage.characters{
-                var character = "\(char)"
-                if let encodedChar = self.code[character]{
-                    encodedMessage += encodedChar
-                }else{
-                    encodedMessage += character
-                    // This should be a space
-                }
-            }
-        }
-        
-        print(encodedMessage)
-//        for char in (self.textField.text?.lowercased().characters)!{
-//            var character = "\(char)"
-//            print(character)
-//            
-//            print(self.code[character])
-//            print(char)
-//        }
-        
-        
-        
-        print(self.textField.text)
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        msgTextField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+    }
+    
+    
+    @IBAction func encodeBtn(_ sender: UIButton) {
+        
+        if let message = msgTextField.text {
+            
+            let newMsg = encodeMsg(message: message)
+            
+            let alert = UIAlertController(title: "Encrypted Message :", message: newMsg, preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "OK", style: .default , handler: {action in
+                self.clearTextField()})
+            
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    func clearTextField() {
+        msgTextField.text = ""
+    }
+    
+    func textField(_ shouldChangeCharactersIntextField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText: NSString = msgTextField.text! as NSString
+        let newText: NSString = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        encodeButton.isEnabled = (newText.length > 0)
+        decodeButton.isEnabled = (newText.length > 0)
+        offsetButton.isEnabled = (newText.length > 0)
+        return true
+    }
+    
+    
+    
+    @IBAction func decodeBtn(_ sender: UIButton) {
+        
+        blurWindow.isHidden = false
+        blurWindow.alpha = 1.0
+        _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: false)
+        print(msgTextField.text)
+        
+        let message = decodeMsg(message: msgTextField.text)
+        displayMessage.text = message
+        
+    }
+    
+    @IBAction func offsetBtn(_ sender: UIButton) {
+        
+        blurWindow.isHidden = false
+        blurWindow.alpha = 1.0
+        _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: false)
+        _ = offset(offsetBy: Int(msgTextField.text!)!)
+//        displayMessage.text = "New dictionary created."
+        clearTextField()
+        print(newAlphabetDict.sorted(by: < ))
+        
+    }
+    
+    
+    func update() {
+        UIView.animate(withDuration: 3.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {self.blurWindow.alpha = 0.0}, completion: {action in self.clearTextField()})
+    }
+    
+    func decodeMsg(message: String?) -> String {
+        var newMessage = ""
+        
+        if let unwrappedMessage = message {
+            for char in unwrappedMessage.lowercased().characters {
+                if char == " " {
+                    newMessage += " "
+                }
+                for (key,value) in code {
+                    if value == "\(char)" {
+                        newMessage += key
+                    }
+                }
+                
+            }
+        }
+        return newMessage
+        
+    }
+    //🐯 🐹🐸🐙🐒 🐔🐙🐹🐰🙉🙊🐒🐶🐙🐹
+    
+    func encodeMsg(message: String?) -> String {
+        var newMessage = ""
+        
+        if let unwrappedMessage = message {
+            for char in unwrappedMessage.lowercased().characters {
+                if char == " " {
+                    newMessage += " "
+                }
+                for (key,value) in code {
+                    if key == "\(char)" {
+                        newMessage += value
+                    }
+                }
+                
+            }
+        }
+        return newMessage
+    }
+    
+    func offset(offsetBy: Int) -> [String : String] {
+        
+        var offsetDictionary = [String:String]()
+        var startOnLetter = alphabet.startIndex + (offsetBy % alphabet.count)
+        var indexOfFirstLetter = 0
 
+        for x in alphabet {
+            
+            if startOnLetter <= (alphabet.count - 1) {
+                
+                offsetDictionary[x] = alphabet[startOnLetter]
+                startOnLetter += 1
+                
+            } else {
+                
+                if indexOfFirstLetter <= offsetBy {
+                    offsetDictionary[x] = alphabet[indexOfFirstLetter]
+                    indexOfFirstLetter += 1
+                }
+                
+            }
+        }
+        displayMessage.text = "New dictionary created."
+        newAlphabetDict = offsetDictionary
+        return newAlphabetDict
+    }
+    
+    
 }
-
